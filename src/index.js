4e8111bd59084aa1a4a3b59c2e50f01069f8511c
Alex Platt <alja.platt@gmail.com>
2022-01-15 14:41:42 +0000

    added Home component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App";
 
+/* This file takes the App component, imported above, and renders it to the DOM, inside the 'root' element.
+
+npm start - runs local development server 
+*/
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
