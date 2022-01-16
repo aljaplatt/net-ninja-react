e628096ef8767891d15cd83bfd1350c9cb91bc64
Alex Platt <alja.platt@gmail.com>
2022-01-16 07:19:06 +0000

    #12 - Reusing Components

--- src/Components/BlogList/index.js
+++ src/Components/BlogList/index.js
@@ -1,7 +1,8 @@
-// blogs being passed in from Home
-const BlogList = ({ blogs }) => {
+// blogs being passed in from Home - we have to do this to as the blogs arr is not defined in this file. By adding blogs on line 2 - it becomes available from the props obj.
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -16,7 +16,11 @@ const Home = () => {
   /* Using the map method we can loop through an array and return some template for each item. The method invokes a callback fn for each item, and returns template for each. */
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+      />
     </div>
   );
 };
@@ -24,3 +28,8 @@ const Home = () => {
 export default Home;
 
 // Line 19 - Blogs arr is being passed down as a prop to BlogList component.
+
+// line 14 - we have duplicated the blog list component, and will pass in new data to the second one, using the filter method & a specified condition. The method invokes a callback fn for each item in the array - if true is returned for the condition, it is kept in the array, else it filters it out. A new array is returned with all the kept (true) items.
+/* 
+For the second component we're going to filter out all posts not from mario. 
+Line 20 - we want to pass in a blog, then return true or false to the condition. */
