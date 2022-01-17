c0b4418dcd917f175859175588a0825356f51165
Alex Platt <alja.platt@gmail.com>
2022-01-17 08:47:30 +0000

    setting up react router

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,19 +2,23 @@
 import Navbar from "../Navbar";
 import Home from "../Home";
 import "./style.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 /* a component is a function, we must return something - usually a JSX template. The component is the exported to be used else where. */
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50; /* React will convert this to a string - the only things React cannot convert are objects & booleans.
- We can also put valid javascript statement straight in the JSX - wrapped in { } - check img for more notes */
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 // exported to global index.js file
